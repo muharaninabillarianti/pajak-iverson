0c1613852ca2738a56f595be3848a752e7e1bdb8
hendro.steven@gmail.com <hendro.steven@gmail.com@7d828e40-23b1-4fab-0f95-e496f3887202>
2012-06-05 08:02:00 +0000

    Mengubah fungsi index

--- trunk/toko/application/controllers/Products.php
+++ trunk/toko/application/controllers/Products.php
@@ -3,7 +3,8 @@
 class Products extends CI_Controller {
 
     public function index() {
-        $this->load->helper('url');
+        //$this->load->helper('url');
+        $this->load->helper('url');        
         return $this->all();
     }
 
