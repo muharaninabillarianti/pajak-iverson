bcbe772898898f01630d802156fb8d590de72425
hendro.steven <hendro.steven@gmail.com>
2012-06-06 03:07:46 +0000

    upadate Customer class

--- toko/application/libraries/entity/Customer.php
+++ toko/application/libraries/entity/Customer.php
@@ -1,49 +1,126 @@
 <?php
 
-
 class Customer {
-   private $id;
-   private $nama;
-   private $alamat;
-   private $telp;
-   private $email;
-   
-   public function __construct($id,$nama,$alamat,$telp,$email) {
-       $this->id=$id;
-       $this->nama = $nama;
-       $this->alamat = $alamat;
-       $this->telp = $telp;
-       $this->email = $email;
-   }
-   
-   public function getId(){
-       return $this->id;
-   }
-   
-   public function setNama($nama){
-       $this->nama = $nama;
-   }
-   public function getNama(){
-       return $this->nama;
-   }
-   public function setAlamat($alamat){
-       $this->alamat = $alamat;
-   }
-   public function getAlamat(){
-       return $this->alamat;
-   }
-   public function setTelp($telp){
-       $this->telp = $telp;
-   }
-   public function getTelp(){
-       return $this->telp;
-   }
-   public function setEmail($email){
-       $this->email = $email;
-   }
-   public function getEmail(){
-       return $this->email;
-   }
+
+    private $id;
+    private $nama;
+    private $alamat;
+    private $telp;
+    private $email;
+
+    public function __construct($id, $nama, $alamat, $telp, $email) {
+        $this->id = $id;
+        $this->nama = $nama;
+        $this->alamat = $alamat;
+        $this->telp = $telp;
+        $this->email = $email;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setNama($nama) {
+        $this->nama = $nama;
+    }
+
+    public function getNama() {
+        return $this->nama;
+    }
+
+    public function setAlamat($alamat) {
+        $this->alamat = $alamat;
+    }
+
+    public function getAlamat() {
+        return $this->alamat;
+    }
+
+    public function setTelp($telp) {
+        $this->telp = $telp;
+    }
+
+    public function getTelp() {
+        return $this->telp;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function save() {
+        $conn = new Connection();
+        $mysqli = $conn->getMysqli();
+        $stmt = $mysqli->prepare("INSERT INTO 
+customer(nama,alamat,telp,email) 
+            VALUES(?,?,?,?)");
+        $stmt->bind_param("ssss", $this->nama, $this->alamat, $this->telp, $this->email);
+        $stmt->execute();
+        $stmt->close();
+        $myqli->close();
+    }
+
+    public function update() {
+        $conn = new Connection();
+        $mysqli = $conn->getMysqli();
+        $stmt = $mysqli->prepare("UPDATE customer SET 
+nama=?,alamat=?,telp=?,
+            email=? WHERE
+           id=?");
+        $stmt->bind_param("ssssi", $this->nama, $this->alamat, $this->telp, $this->email, $this->id);
+        $stmt->execute();
+        $stmt->close();
+        $mysqli->close();
+    }
+
+    public static function delete($id) {
+        $conn = new Connection();
+        $mysqli = $conn->getMysqli();
+        $stmt = $mysqli->prepare("DELETE FROM customer WHERE id=?");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $stmt->close();
+        $mysqli->close();
+    }
+
+    public static function load($id) {
+        $conn = new Connection();
+        $mysqli = $conn->getMysqli();
+        $stmt = $mysqli->prepare("SELECT id,nama,alamat,telp,email FROM 
+customer WHERE id=?");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $stmt->bind_result($id, $nama, $alamat, $telp, $eamil);
+        $customer = null;
+        while ($r = $stmt->fetch()) {
+            $customer = new Customer($id, $nama, $alamat, $telp, $email);
+        }
+        $stmt->close();
+        $mysqli->close();
+        return $customer;
+    }
+
+    public static function loads() {
+        $conn = new Connection();
+        $mysqli = $conn->getMysqli();
+        $stmt = $mysqli->prepare("SELECT id,nama,alamat,telp,email FROM 
+customer");
+        $stmt->execute();
+        $stmt->bind_result($id, $nama, $alamat, $telp, $eamil);
+        $customers = array();
+        while ($r = $stmt->fetch()) {
+            $customer = new Customer($id, $nama, $alamat, $telp, $email);
+            array_push($customers, $customer);
+        }
+        $stmt->close();
+        $mysqli->close();
+        return $customers;
+    }
+
 }
 
 ?>
