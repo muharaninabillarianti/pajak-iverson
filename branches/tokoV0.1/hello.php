603b89dccb60cacc528b91d65417f609b63b7889
hendro.steven@gmail.com <hendro.steven@gmail.com@7d828e40-23b1-4fab-0f95-e496f3887202>
2013-10-28 08:00:39 +0000

    release toko v0.1

---
+++ branches/tokoV0.1/hello.php
@@ -0,0 +1,18 @@
+<?php
+echo "ini perubahan baru"
+echo "helo world";
+echo "coba nulis";
+echo "febi";
+echo "febi lg";
+echo" tes"
+echo "hendro"; echo "cilubakikuk"
+echo "febby terus"
+?>
+/* End of file hello.php */
+<<<<<<< .mine
+ok deh
+ok lagi ahhh 
+seeeep
+hasan
+=======
+>>>>>>> .r37
