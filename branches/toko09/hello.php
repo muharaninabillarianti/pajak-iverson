2a9f5fff934b6e4bc7f35a63d1180435a7b6ef53
antostanko@gmail.com <antostanko@gmail.com@7d828e40-23b1-4fab-0f95-e496f3887202>
2013-10-28 08:17:47 +0000

    mau naro album

---
+++ branches/toko09/hello.php
@@ -0,0 +1,12 @@
+<?php
+echo "ini perubahan baru"
+echo "helo world";
+echo "coba nulis";
+echo "febi";
+echo "febi lg";
+echo" tes"
+echo "hendrion"; echo "cilubakikuk"
+;?>
+/* End of file hello.php */
+ok deh
+ok lagi ahhh 
---
+++ branches/toko09/hello_kitty.php
@@ -0,0 +1,13 @@
+<?php
+
+phpinfo(); echo "ini perubahan baru";
+echo "helo world";
+echo "coba nulis";
+echo "febi";
+echo "febi lg";
+echo" tes"
+echo "hendrion"; echo "cilubakikuk ASBAK"
+echo "tambah lagi cuy"
+echo "apa ini"
+echo "apa lg ini"
+?>
