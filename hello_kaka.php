23e66bdf169fa1aa44754647031aceb855284448
imron.tux <imron.tux@gmail.com>
2013-10-29 04:11:44 +0000

    update hello_kaka.php

--- hello_kaka.php
+++ hello_kaka.php
@@ -5,11 +5,11 @@ echo "helo world";
 echo "coba nulis";
 echo "febi";
 echo "febi lg";
-echo" tes"
-echo "hendrion"; echo "cilubakikuk ASBAK"
-echo "tambah lagi cuy"
-echo "apa ini"
-echo "apa lg ini"
-echo "acak2"
-echo "nambah file buat revert"
+echo" tes";
+echo "hendrion"; echo "cilubakikuk ASBAK";
+echo "tambah lagi cuy";
+echo "apa ini";
+echo "apa lg ini";
+echo "acak2";
+echo "nambah file buat revert";
 ?>
