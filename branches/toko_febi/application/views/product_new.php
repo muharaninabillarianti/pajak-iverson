15c60b09aa85c752d64d0fbb103d31566f2c0d21
febi.indriani@gmail.com <febi.indriani@gmail.com@7d828e40-23b1-4fab-0f95-e496f3887202>
2013-10-29 03:58:04 +0000

    buat baru

---
+++ branches/toko_febi/application/views/product_new.php
@@ -0,0 +1,19 @@
+<h3>Insert New Product</h3>
+<?php echo form_open_multipart('/products/insert'); ?>
+<h5>Kode</h5>
+<?php echo form_error('kode'); ?>
+<input type="text" name="kode" value="<?php echo set_value('kode'); ?>" size="20" />
+
+<h5>Nama</h5>
+<?php echo form_error('nama'); ?>
+<input type="text" name="nama" value="<?php echo set_value('nama'); ?>" size="50" />
+
+<h5>Price</h5>
+<?php echo form_error('price'); ?>
+<input type="text" name="price" value="<?php echo set_value('price'); ?>" size="50" />
+
+<h5>Image</h5>
+<input type="file" name="userfile" size="20" />
+
+<div><input type="submit" value="Submit" /></div>
+</form>
